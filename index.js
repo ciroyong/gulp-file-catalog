e287c0f4e1e893b3718cd64b1569f451e20ba82d
ciroyong <ciroyong@hotmail.com>
2016-01-15 23:11:33 +0800

    by ciroyong

--- index.js
+++ index.js
@@ -1,44 +1,40 @@
 'use strict';
 
-var minimatch, through, Path, gutil, PluginError, op, chunk;
+var $minimatch, $through2, $path, $gutil, $PluginError, _op, chunk;
 
-through = require('through2');
-minimatch = require('minimatch');
-Path = require('path');
-gutil = require('gulp-util');
-PluginError = gutil.PluginError;
+$through2 = require('through2');
+$minimatch = require('minimatch');
+$path = require('path');
+$gutil = require('gulp-util');
+$PluginError = gutil.PluginError;
+_op = Object.prototype;
 chunk = {};
-op = Object.prototype;
 
-function hasProp(obj, name) {
-    return op.hasOwnProperty.call(obj, name);
+function __hasProp(obj, name) {
+    return _op.hasOwnProperty.call(obj, name);
 }
 
 module.exports = function(opt, handler) {
-    var filename, cwd, filter, exclude;
+    var out, cwd, exclude;
 
     cwd = "", filter = false, exclude = false;
 
     if(typeof opt === "string") {
-        filename = opt;
+        out = opt;
     }
 
     if(typeof opt === "object") {
-        if(!hasProp(opt, "out")) {
+        if(!_hasProp(opt, "out")) {
             throw new PluginError('gulp-file-catalog', 'Missing options "out"');
         }
 
-        filename = opt.out;
+        out = opt.out;
 
-        if(hasProp(opt, "cwd")) {
+        if(_hasProp(opt, "cwd")) {
             cwd = opt.cwd;
         }
 
-        if(hasProp(opt, "filter")) {
-            filter = opt.filter + '';
-        }
-
-        if(hasProp(opt, "exclude")) {
+        if(_hasProp(opt, "exclude")) {
             if (typeof opt.exclude === "string") {
                 exclude = [opt.exclude];
             }
@@ -48,55 +44,49 @@ module.exports = function(opt, handler) {
             }
         }
     }
+    /***
+    Path.parse('/home/user/dir/file.txt')
+    @property: root, "/",
+    @property: dir, "/home/user/dir",
+    @property: base, "file.txt",
+    @property: ext, ".txt",
+    @property: name, "file"
+    ***/
+    return $through2({objectMode: true, allowHalfOpen: false}, function(buffer, encoding, callback) {
+        var basename, ref, root, dir, base, ext, name, relative;
 
-    function parsePath(file) {
-        var path, extname, basename, relative;
-
-        path  = file.path;
-        extname = Path.extname(path);
-
-        return {
-            path: path,
-            relative: file.relative,
-            extname: Path.extname(path),
-            basename: Path.basename(path, extname)
-        }
-    }
-
-    return through.obj(function(file, enc, cb) {
-        var basename;
-
-        if(!file.stat.isFile()) {
-            cb();
+        if(!buffer.stat.isFile()) {
+            callback();
             return;
         }
 
-        file = parsePath(file);
+        ref = $path.parse(buffer.path);
+        root = ref.root, dir = ref.dir, base = ref.base, ext = ref.ext, name = ref.name;
 
         if(exclude) {
             for (var i = exclude.length - 1; i >= 0; i--) {
-                if(minimatch(file.relative, exclude[i])) {
-                    cb();
+                if(minimatch(dir, exclude[i])) {
+                    callback();
                     return;
                 }
             }
         }
 
-        basename = file.basename;
-
         if(typeof handler === "function") {
-            basename = handler(file);
+            name = handler(name);
         }
 
-        chunk[basename] = cwd + file.relative;
+        relative = $path.relative(cwd, path);
 
-        cb();
-    }, function end(cb) {
+        chunk[name] = relative.split($path.sep);
+
+        callback();
+    }, function (callback) {
         var file;
 
-        file = new gutil.File({path: filename});
+        file = new gutil.File({path: out});
         file.contents = new Buffer(JSON.stringify(chunk));
         this.push(file);
-        cb();
+        callback();
     });
 };
